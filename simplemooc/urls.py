df42b8b12c7104d4ccfa505216e159f1df8310d2
RodrigoMMSa <rodrigomunizsa@gmail.com>
2019-07-12 14:55:58 -0300

    Organizing code - using namespace and url

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .core import views
-
+from django.urls import path, include
+from .core import urls
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
+    path('', include(urls, namespace='core')),
     path('admin/', admin.site.urls),
 ]
