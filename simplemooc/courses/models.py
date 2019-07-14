e8d2884d4a0633cd6c50264c6bbd55a6a554bdac
RodrigoMMSa <rodrigomunizsa@gmail.com>
2019-07-14 17:06:38 -0300

    Adding Search class on models and change Courses to Course

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 
-class Courses(models.Model):
+class CourseManager(models.Manager):
+    def search(self, query):
+        return self.get_queryset().filter(models.Q(name__icontains=query) | models.Q(description__icontains=query))
+
+
+class Course(models.Model):
 
     name = models.CharField('Name', max_length=100)
     slug = models.SlugField('Shortcut')
     description = models.TextField('Description', blank=True)
     start_date = models.DateField('Start Date', null=True, blank=True)
-    image = models.ImageField(upload_to='courses/images', verbose_name='Image')
+    image = models.ImageField(upload_to='courses/images', verbose_name='Image', null=True, blank=True)
     created_at = models.DateTimeField('Created_at', auto_now_add=True)
     updated_at = models.DateTimeField('Last Modified at', auto_now=True)
+    objects = CourseManager()
