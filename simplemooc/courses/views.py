62feb33d4b1d5f0c7e117eaba48488e3f700d68a
RodrigoMMSa <rodrigomunizsa@gmail.com>
2019-08-02 15:54:20 -0300

    New site Form for questions

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Course
+from .forms import ContactCourse
 
 
 def index(request):
@@ -11,6 +12,10 @@ def index(request):
 
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
-    context = {'course': course}
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+    else:
+        form = ContactCourse()
+    context = {'course': course, 'form': form}
     template_name = 'courses/details.html'
     return render(request, template_name, context)
