5b2aaaf4f9bfbe0c8a1649e2b49fa62fe246cdb0
RodrigoMMSa <rodrigomunizsa@gmail.com>
2019-09-06 14:01:41 +1000

    login page

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
+from . import views
+
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('register/', views.register, name='register'),
 ]
