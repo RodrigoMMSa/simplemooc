73b516653cf8e22ea8792704c62894db857bf0bd
RodrigoMMSa <rodrigomunizsa@gmail.com>
2019-09-24 12:01:18 +1000

    Login and Logout

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from . import views
 
 
@@ -8,4 +9,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('register/', views.register, name='register'),
+    path('logout/', LogoutView.as_view(next_page='core:home'), name='logout'),
 ]
+
