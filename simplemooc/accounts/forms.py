8827a4db340035fe9d8e68aa887caba825bde3b7
RodrigoMMSa <rodrigomunizsa@gmail.com>
2019-09-25 14:27:51 +1000

    Creating password reset

--- simplemooc/accounts/forms.py
+++ simplemooc/accounts/forms.py
@@ -7,14 +7,26 @@ User = get_user_model()
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(label='E-mail')
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 != password2:
+            raise forms.ValidationError(self.error_messages['password_mismatch'], code='password_mismatch',)
+        return password2
 
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
+        user.set_password(self.cleaned_data['password1'])
         if commit:
             user.save()
         return user
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
 
 
 class EditAccountForm(forms.ModelForm):
--- simplemooc/accounts/models.py
+++ simplemooc/accounts/models.py
@@ -1,10 +1,17 @@
+import re
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.core import validators
+from django.conf import settings
 
 
 class User(AbstractBaseUser, PermissionsMixin):
 
-    username = models.CharField('Username', max_length=30, unique=True)
+    username = models.CharField(
+        'Username', max_length=30, unique=True,
+        validators=[validators.RegexValidator(
+            re.compile('^[\w.@+-]+$'), "username can only contain letters, digits or '@/./+/-/_'", 'invalid')])
     email = models.EmailField('E-mail', unique=True)
     name = models.CharField('Name', max_length=100, blank=True)
     is_active = models.BooleanField('Is it active?', blank=True, default=True)
@@ -28,3 +35,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+
+
+class PasswordReset(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, verbose_name='User', related_name='password_reset', on_delete=models.CASCADE)
+    key = models.CharField('Chave', max_length=100, unique=True)
+    created_at = models.DateTimeField('Created at', auto_now_add=True)
+    confirmed = models.BooleanField('Confirmed?', default=False, blank=True)
+
+    def __str__(self):
+        return '{0} in {1}'.format(self.user, self.created_at)
+
+    class Meta:
+        verbose_name = 'New Password'
+        verbose_name_plural = 'New Passwords'
+        ordering = ['-created_at']
