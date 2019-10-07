241e3d222201b124227279f6f0bac47e060dcb87
RodrigoMMSa <rodrigomunizsa@gmail.com>
2019-10-07 12:18:44 +1100

    Decorators -- Finished

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Course, Enrollment, Announcement
 from .forms import ContactCourse, CommentForm
+from .decorators import enrollment_required
 
 
 def index(request):
