3e94a8e623b4e6f3f5d58bee6cc69d5f3c0cd530
RodrigoMMSa <rodrigomunizsa@gmail.com>
2019-10-07 12:28:34 +1100

    fixing dashboard

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -56,26 +56,18 @@ def undo_enrollment(request, slug):
 
 
 @login_required
+@enrollment_required
 def announcements(request, slug):
-    course = get_object_or_404(Course, slug=slug)
-    if not request.user.is_staff:
-        enrollment = get_object_or_404(Enrollment, user=request.user, course=course)
-        if not enrollment.is_approved():
-            messages.error(request, 'Your enrollment is pendent')
-            return redirect('accounts:dashboard')
+    course = request.course
     template = 'courses/announcements.html'
     context = {'course': course, 'announcements': course.announcements.all}
     return render(request, template, context)
 
 
 @login_required
+@enrollment_required
 def show_announcement(request, slug, pk):
-    course = get_object_or_404(Course, slug=slug)
-    if not request.user.is_staff:
-        enrollment = get_object_or_404(Enrollment, user=request.user, course=course)
-        if not enrollment.is_approved():
-            messages.error(request, 'Your enrollment is pendent')
-            return redirect('accounts:dashboard')
+    course = request.course
     announcement = get_object_or_404(course.announcements.all(), pk=pk)
     form = CommentForm(request.POST or None)
     if form.is_valid():
